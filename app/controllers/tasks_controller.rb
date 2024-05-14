cc0c5ffabcf61cd2a0ba31705388c0e01cdd56e6
David Wenk <davidwenk@icloud.com>
2024-05-14 13:36:39 +0200

    add delete task function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path, status: :see_other
   end
 
   def edit
@@ -24,6 +25,13 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path, status: :see_other
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,7 @@ Rails.application.routes.draw do
   get "tasks/:id/edit", to: "tasks#edit"
   patch "tasks/:id", to: "tasks#update"
 
+  # delete 
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
